a433412a62854006a5c2fcc9cae2028bbd1a9617
Stephen Tam <sdtam@outlook.com>
2017-11-12 20:08:17 -0500

    add user model and tests

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
+"""
+Models for api app
+"""
+
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """ A user of the donor app """
+
+    username = models.CharField(max_length=200)
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+"""
+Tests for api app
+"""
 
-# Create your tests here.
+from django.test import TestCase
+from .models import User
+
+class UserModelTestCase(TestCase):
+    """ Test suite for User model """
+
+    def setUp(self):
+        """ define reusable test variables """
+        self.expected_username = "stam"
+        self.user = User(username=self.expected_username)
+        return
+
+    def test_that_user_has_username(self):
+        """ tests that the user has a username """
+        self.assertEqual(self.user.username, self.expected_username)
+        return
+
+    def test_that_can_create_user(self):
+        """ tests that a user can be created and saved """
+        count_before_save = User.objects.count()
+        self.user.save()
+        count_after_save = User.objects.count()
+
+        self.assertEqual(count_after_save - count_before_save, 1)
+        return
